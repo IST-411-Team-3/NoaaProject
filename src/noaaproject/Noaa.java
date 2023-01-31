e67502dbd1071b60363a52a0bc23a1bd577d2808
AJ <AJ@Mother-Base>
2023-01-31 14:24:39 -0500

    Initial Commit

---
+++ src/noaaproject/Noaa.java
@@ -0,0 +1,16 @@
+package noaaproject;
+
+/**
+ *
+ * @author AJ Germani
+ */
+public class Noaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
